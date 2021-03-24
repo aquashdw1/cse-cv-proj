0e4265597185b71389d43f0725af4e14a650773f
aquashdw <aquashdw@gmail.com>
2021-03-24 13:54:46 +0900

    gradient histogram retrieval function

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import numpy as np
 
-from utils.image_reader import ImageReader
 from utils.point_extension import PointExtractor
+
+from project1_hog.gradient_histogram import get_grad_hist
 
 
 if __name__ == '__main__':
@@ -11,21 +12,26 @@ if __name__ == '__main__':
         name="test1"
     )
 
-    image_first.set_points(4)
-    image_first.imshow()
+    # image_first.set_points(4)
+    # image_first.imshow()
 
     image_second = PointExtractor(
         image_filename="project1_hog/2nd.jpg",
         name="test2"
     )
 
-    image_second.set_points(4)
-    image_second.imshow()
+    image_second.set_points(1)
+    get_grad_hist(image_second.image_array, image_second.points[0])
 
-    image_third = ImageReader(
-        image_array=np.concatenate(
-            (image_first.image_array, image_second.image_array),
-            axis=1),
-        name="concatenated"
-    )
-    image_third.imshow()
+    # image_third = ImageReader(
+    #     image_array=np.concatenate(
+    #         (image_first.image_array, image_second.image_array),
+    #         axis=1),
+    #     name="concatenated"
+    # )
+    # image_third.imshow()
+    #
+    # merge_fs = ImageMerge(
+    #     image_arrays=[image_first.image_array, image_second.image_array]
+    # )
+    # merge_fs.preprocess()
---
+++ project1_hog/gradient_histogram.py
@@ -0,0 +1,33 @@
+import cv2
+import math
+import numpy as np
+
+
+def get_grad_hist(img_array, point, distance=3, bins=20):
+    sobel_x = cv2.Sobel(img_array, cv2.CV_32F, 1, 0, 1)
+    sobel_y = cv2.Sobel(img_array, cv2.CV_32F, 0, 1, 1)
+
+    raw_mag, raw_ang = cv2.cartToPolar(sobel_x, sobel_y, angleInDegrees=True)
+
+    pixel_area_size = distance * 2 + 1
+    mag_array = np.zeros((pixel_area_size, pixel_area_size))
+    ang_array = np.zeros((pixel_area_size, pixel_area_size))
+
+    for i in range(pixel_area_size):
+        for j in range(pixel_area_size):
+            mag_array[i, j] = max(raw_mag[point[1] - 2 + i, point[0] - 2 + j])
+            ang_array[i, j] = max(raw_ang[point[1] - 2 + i, point[0] - 2 + j])
+
+    unit_angle = 360 / bins
+    hist_list_raw = [0] * bins
+
+    for i in range(pixel_area_size):
+        for j in range(pixel_area_size):
+            target_bin = int(ang_array[i, j] / unit_angle)
+            hist_list_raw[target_bin] += mag_array[i, j]
+
+    divisor = max(hist_list_raw)
+    ret_list = []
+    for i in hist_list_raw:
+        ret_list.append(round((i / divisor) * 100, 3))
+    return ret_list
